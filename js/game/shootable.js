f4de77032d1095a8490de88475d1866a7d3738fb
hannah-nanadev <hannahkellett2@gmail.com>
2023-12-14 10:37:35 +0000

    alien no longer affected by gravity but no brain

--- js/game/shootable.js
+++ js/game/shootable.js
@@ -19,6 +19,75 @@ class Shootable extends GameObject {
         this.movementLimit = 100;
         this.movingRight = true;
         this.movingUp = true;
+        this.movingHorizontal = true;
+    }
+
+    update(deltaTime)
+    {
+        const physics = this.getComponent(Physics);
+
+        //Checks for directions
+        if(this.movingHorizontal)
+        { //Check if moving horizontally
+            if(this.movingRight)
+            { //Move right if not at limit when initially moving right
+                if(this.movementDistance < this.movementLimit)
+                {
+                    physics.velocity.x = 100;
+                    this.movementDistance += Math.abs(physics.velocity.x) * deltaTime;
+                }
+                else
+                { //Otherwise, turn left
+                    this.movingRight = false;
+                    this.movementDistance = 0;
+                }
+            }
+            else
+            { //Move left if not at limit when initially moving left
+                if(this.movementDistance < this.movementLimit)
+                {
+                    physics.velocity.x = -100;
+                    this.movementDistance += Math.abs(physics.velocity.x) * deltaTime;
+                }
+                else
+                { //Otherwise, turn right
+                    this.movingRight = true;
+                    this.movementDistance = 0;
+                }
+            }
+        }
+        else
+        { //If moving vertically...
+            if(this.movingUp)
+            { //Move up if not at limit when initially moving up
+                if(this.movementDistance < this.movementLimit)
+                {
+                    physics.velocity.y = 100;
+                    this.movementDistance += Math.abs(physics.velocity.y) * deltaTime;
+                }
+                else
+                { //Otherwise, turn downwards
+                    this.movingUp = false;
+                    this.movementDistance = 0;
+                }
+            }
+            else
+            { //Move down if not at limit when initially moving down
+                if(this.movementDistance < this.movementLimit)
+                {
+                    physics.velocity.y = -100;
+                    this.movementDistance += Math.abs(physics.velocity.y) * deltaTime;
+                }
+                else
+                { //Otherwise, turn upwards
+                    this.movingUp = true;
+                    this.movementDistance = 0;
+                }
+            }
+        }
+    
+
+        
     }
 
 }
